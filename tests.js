4007f7e52c84ce308216dadb7b65a076a11e2569
osseko <253503304+osseko@users.noreply.github.com>
2026-07-01 22:14:33 +0800

    more testing

--- tests.js
+++ tests.js
@@ -1,95 +1,104 @@
-//capitalize
+// //capitalize
 
-function capitalize(toCapitalize){
-    const newCapitalize = toCapitalize[0].toUpperCase() + toCapitalize.slice(1);
-    return newCapitalize;
-}
+// export function capitalize(toCapitalize){
+//     const newCapitalize = toCapitalize[0].toUpperCase() + toCapitalize.slice(1);
+//     return newCapitalize;
+// };
 
-console.log(capitalize("rebel"));
-//reverseString
+// //reverseString
 
-function reverseString(toReverse){
-    const newReverse = toReverse.split('').reverse().join('');
-    return newReverse;
-}
+// export function reverseString(toReverse){
+//     const newReverse = toReverse.split('').reverse().join('');
+//     return newReverse;
+// };
 
-console.log(reverseString("revel"));
-//calculator
+// //calculator
 
-//number1, number2, operation
-
-function calculator(a, b, operation){
-    //add, subtract, divide, multiply
-    if(operation === "add"){
-        return a + b;
-    }if(operation === "subtract"){
-        return a - b;
-    }if(operation === "divide"){
-        return a / b;
-    }if(operation === "multiply"){
-        return a * b;
-    }
-}
-
-console.log(calculator(1,2, "subtract"));
+// export function calculator(a, b, operation){
+//     //add, subtract, divide, multiply
+//     if(operation === "add"){
+//         return a + b;
+//     }if(operation === "subtract"){
+//         return a - b;
+//     }if(operation === "divide"){
+//         return a / b;
+//     }if(operation === "multiply"){
+//         return a * b;
+//     }
+// };
 
 //caesarCipher
+// /\W+/g
+function caesarCipher(toEncrypt, index){
 
+ const alphabet = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x","Y","y","Z","z"]
 
- const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
- const cipher =  ["d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c"]
+//  const cipher =  ["d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c"]
 
  let indexes = [];
-
- const sampString = "sampleString";
-
- const newString = sampString.toLowerCase().split("");
-
- newString.forEach((letter) => indexes.push(alphabet.indexOf(letter)));
-
- console.log(indexes);
-
  let cipherArr = [];
- const newCipher = indexes.forEach((value) => cipherArr.push(cipher[value]));
 
- const encryptedArr = cipherArr.join("");
+ const adjustment = index * 2;
+ const newString = toEncrypt.split("");
 
- console.log("cipher", encryptedArr);
-
-function caesarCipher(toEncrypt){
-   
-
-
-}
-
-//analyzeArray
-
-        //average(sum/totalnumber(length))/min(lowest)/max(highest)/length
-
-function analyzeArray(setOfArray){
-
-    const setLength = setOfArray.length;
-
-    const arraySum = setOfArray.reduce((sum, current) => sum + current, 0)
-    const setAverage = arraySum / setLength;
-
-    const lowest = setOfArray.sort((a, b) => a - b);
-    const setMin = lowest[0];
-    
-    const highest = setOfArray.sort((a, b) =>  b - a);
-    const setMax = highest[0];
-   
-    return{
-        average: setAverage,
-        min: setMin,
-        max: setMax,
-        length: setLength 
+    function findAdjustment(){
+        //if adjustment > 52 : (adjustment - 52) => alphabet[0 + result]
     }
 
-}
+ console.log(newString);
+
+ newString.forEach((letter) => 
+    {if(letter.match(/[a-zA-Z]+/g)){
+        indexes.push(alphabet.indexOf(letter))
+    }else{
+        indexes.push(letter);
+    }});
+
+    console.log("index", indexes);
+
+  const newCipher = indexes.forEach((value) => 
+    {if(value.length){
+        cipherArr.push(value)
+     }else{
+        cipherArr.push(alphabet[value + adjustment])
+    }});
+
+ console.log(cipherArr)
+
+ const encryptedArr = cipherArr.join("");
+ 
+ return encryptedArr;
+
+};
+
+console.log(caesarCipher("TO beyond", 3));
+//analyzeArray
+
+// export function analyzeArray(setOfArray){
+
+//     const setLength = setOfArray.length;
+
+//     const arraySum = setOfArray.reduce((sum, current) => sum + current, 0)
+//     const setAverage = arraySum / setLength;
+
+//     const lowest = setOfArray.sort((a, b) => a - b);
+//     const setMin = lowest[0];
+    
+//     const highest = setOfArray.sort((a, b) =>  b - a);
+//     const setMax = highest[0];
+   
+//     return{
+//         average: setAverage,
+//         min: setMin,
+//         max: setMax,
+//         length: setLength 
+//     }
+
+// };
 
 
-console.log(analyzeArray([5, 10, 16, 8,]));
+// export function sum(a, b) {
+//  return a + b  
+// };
 
 
--- tests.test.js
+++ tests.test.js
@@ -1,5 +1,47 @@
-const tests = require('./tests');
+import {describe, expect, test} from '@jest/globals';
+import {sum} from "./tests.js";
+import {capitalize} from "./tests.js";
+import { reverseString } from './tests.js';
+import { calculator } from './tests.js';
+import { caesarCipher } from './tests.js';
+import { analyzeArray } from './tests.js';
 
-test('capitalizes first letter of word', () => {
-    expect(capitalize('hello')).toBe('Hello')
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('capitalize first letter', () => {
+    expect(capitalize('hello')).toBe('Hello');
+})
+
+test('reverses string', () => {
+    expect(reverseString('hello world')).toBe('dlrow olleh');
+})
+
+test('calculator to add', () =>{
+    expect(calculator(2, 2, "add")).toBe(4);
+})
+
+test('calculator to subract', () => {
+    expect(calculator(20, 15, "subtract")).toBe(5);
+})
+
+test('calculator to divide', () =>{
+    expect(calculator(16, 8, "divide")).toBe(2);
+})
+
+test('calculator to multiply', () =>{
+    expect(calculator(8, 8, "multiply")).toBe(64);
+})
+
+test('encrypts message', () => {
+    expect(caesarCipher('Hello, World', 3)).toBe('Khoor, Zruog');
+})
+
+let object;
+
+test('gives array averages, min, max, and length', () =>{
+    expect(analyzeArray([1,8,3,4,2,6]))
+        .toBe(object = {"average": 4, "min": 1, "max": 8, "length": 6})
 })
